c66c779ca8efa36729bcef14aef9285fb685cd64
rapbot22 <aayush22harsh@gmail.com>
2024-11-06 15:48:31 +0530

    added a comment

--- app/src/main/java/com/lips/fitquest/MainActivity.java
+++ app/src/main/java/com/lips/fitquest/MainActivity.java
@@ -13,6 +13,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        //aayush
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
